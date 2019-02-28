624d04de5b825fb03b6befd7290fbc24253830e4
Michael Doye <mdoye@mobiquityinc.com>
2019-02-28 21:02:57 +0100

    chore(demo): :rocket: deploy new version

--- doc/js/menu-wc.js
+++ doc/js/menu-wc.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 customElements.define('compodoc-menu', class extends HTMLElement {
     constructor() {
         super();
@@ -11,98 +12,95 @@ customElements.define('compodoc-menu', class extends HTMLElement {
     }
 
     render(isNormalMode) {
-        let tp = lithtml.html(`<nav>
-    <ul class="list">
-        <li class="title">
-            <a href="index.html" data-type="index-link">mat-progress-buttons documentation</a>
-        </li>
-        <li class="divider"></li>
-        ${ isNormalMode ? `<div id="book-search-input" role="search">
-    <input type="text" placeholder="Type to search">
-</div>
-` : '' }
-        <li class="chapter">
-            <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
-            <ul class="links">
-                    <li class="link">
-                        <a href="overview.html" data-type="chapter-link">
-                            <span class="icon ion-ios-keypad"></span>Overview
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">mat-progress-buttons documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                        <li class="link">
+                            <a href="overview.html" data-type="chapter-link">
+                                <span class="icon ion-ios-keypad"></span>Overview
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="index.html" data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>README
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="changelog.html"  data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>CHANGELOG
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="license.html"  data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>LICENSE
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="dependencies.html" data-type="chapter-link">
+                                <span class="icon ion-ios-list"></span>Dependencies
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                    <li class="chapter modules">
+                        <a data-type="chapter-link" href="modules.html">
+                            <div class="menu-toggler linked" data-toggle="collapse" ${ isNormalMode ?
+                                'data-target="#modules-links"' : 'data-target="#xs-modules-links"' }>
+                                <span class="icon ion-ios-archive"></span>
+                                <span class="link-name">Modules</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
                         </a>
-                    </li>
-                    <li class="link">
-                        <a href="index.html" data-type="chapter-link">
-                            <span class="icon ion-ios-paper"></span>README
-                        </a>
-                    </li>
-                    <li class="link">
-                            <a href="changelog.html"
-                        data-type="chapter-link">
-                            <span class="icon ion-ios-paper"></span>CHANGELOG
-                        </a>
-                    </li>
-                    <li class="link">
-                            <a href="license.html"
-                        data-type="chapter-link">
-                            <span class="icon ion-ios-paper"></span>LICENSE
-                        </a>
-                    </li>
-                    <li class="link">
-                        <a href="dependencies.html"
-                            data-type="chapter-link">
-                            <span class="icon ion-ios-list"></span>Dependencies
-                        </a>
-                    </li>
-            </ul>
-        </li>
-        <li class="chapter modules">
-            <a data-type="chapter-link" href="modules.html">
-                <div class="menu-toggler linked" data-toggle="collapse"
-                    ${ isNormalMode ? 'data-target="#modules-links"' : 'data-target="#xs-modules-links"' }>
-                    <span class="icon ion-ios-archive"></span>
-                    <span class="link-name">Modules</span>
-                    <span class="icon ion-ios-arrow-down"></span>
-                </div>
-            </a>
-            <ul class="links collapse"
-            ${ isNormalMode ? 'id="modules-links"' : 'id="xs-modules-links"' }>
-                    <li class="link">
-                        <a href="modules/MatProgressButtonsModule.html" data-type="entity-link">MatProgressButtonsModule</a>
-                            <li class="chapter inner">
-                                <div class="simple menu-toggler" data-toggle="collapse"
-                                    ${ isNormalMode ? 'data-target="#components-links-module-MatProgressButtonsModule-abaad944a4b8cf571097ac8cf7433f0e"' : 'data-target="#xs-components-links-module-MatProgressButtonsModule-abaad944a4b8cf571097ac8cf7433f0e"' }>
-                                    <span class="icon ion-md-cog"></span>
-                                    <span>Components</span>
-                                    <span class="icon ion-ios-arrow-down"></span>
-                                </div>
-                                <ul class="links collapse"
-                                    ${ isNormalMode ? 'id="components-links-module-MatProgressButtonsModule-abaad944a4b8cf571097ac8cf7433f0e"' : 'id="xs-components-links-module-MatProgressButtonsModule-abaad944a4b8cf571097ac8cf7433f0e"' }>
-                                        <li class="link">
-                                            <a href="components/MatBarButtonComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules">MatBarButtonComponent</a>
-                                        </li>
-                                        <li class="link">
-                                            <a href="components/MatSpinnerButtonComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules">MatSpinnerButtonComponent</a>
-                                        </li>
-                                </ul>
+                        <ul class="links collapse" ${ isNormalMode ? 'id="modules-links"' : 'id="xs-modules-links"' }>
+                            <li class="link">
+                                <a href="modules/MatProgressButtonsModule.html" data-type="entity-link">MatProgressButtonsModule</a>
+                                    <li class="chapter inner">
+                                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
+                                            'data-target="#components-links-module-MatProgressButtonsModule-abaad944a4b8cf571097ac8cf7433f0e"' : 'data-target="#xs-components-links-module-MatProgressButtonsModule-abaad944a4b8cf571097ac8cf7433f0e"' }>
+                                            <span class="icon ion-md-cog"></span>
+                                            <span>Components</span>
+                                            <span class="icon ion-ios-arrow-down"></span>
+                                        </div>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-MatProgressButtonsModule-abaad944a4b8cf571097ac8cf7433f0e"' :
+                                            'id="xs-components-links-module-MatProgressButtonsModule-abaad944a4b8cf571097ac8cf7433f0e"' }>
+                                            <li class="link">
+                                                <a href="components/MatBarButtonComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">MatBarButtonComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/MatSpinnerButtonComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">MatSpinnerButtonComponent</a>
+                                            </li>
+                                        </ul>
+                                    </li>
                             </li>
+                </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#interfaces-links"' :
+                            'data-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse" ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/MatProgressButtonOptions.html" data-type="entity-link">MatProgressButtonOptions</a>
+                            </li>
+                        </ul>
                     </li>
             </ul>
-        </li>
-        <li class="chapter">
-            <div class="simple menu-toggler" data-toggle="collapse"
-                ${ isNormalMode ? 'data-target="#interfaces-links"' : 'data-target="#xs-interfaces-links"' }>
-                <span class="icon ion-md-information-circle-outline"></span>
-                <span>Interfaces</span>
-                <span class="icon ion-ios-arrow-down"></span>
-            </div>
-            <ul class="links collapse"
-            ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
-                    <li class="link">
-                        <a href="interfaces/MatProgressButtonOptions.html" data-type="entity-link">MatProgressButtonOptions</a>
-                    </li>
-            </ul>
-        </li>
-    </ul>
-</nav>`);
+        </nav>
+        `);
         this.innerHTML = tp.strings;
     }
 });
